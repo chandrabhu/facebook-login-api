50d7ecbd90118f1a3ef3e4dc49432a16133f80b8
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-11-28 22:09:07 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,7 @@
 <?php
- 
-require_once "config.php";
+require_once "./config.php";
 
-if(isset($_SESSION['google_access_token']) || isset($_SESSION['user_id']) || isset($_SESSION['fb_access_token']) ){
-
-unset($_SESSION['google_access_token']);
-$gClient->revokeToken();
-
+if( isset($_SESSION['user_id']) || isset($_SESSION['fb_access_token']) ){
 unset($_SESSION['user_id']);
 unset($_SESSION['fb_access_token']);
 session_destroy();
